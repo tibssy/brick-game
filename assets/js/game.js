50e65652672f4beb072b5587fe77300fcbc85a3c
Norbert Tiborcz <norbert.tiborcz@gmail.com>
2024-07-19 03:05:17 +0000

    Test deployment after the previous deployment failed

--- assets/js/game.js
+++ assets/js/game.js
@@ -38,7 +38,6 @@ export function startGame() {
             });
             break;
         default:
-            console.log("No game selected");
             alert("No game selected");
             break;
     }
